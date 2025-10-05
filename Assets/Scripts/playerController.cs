7ca42670993ff77bee0c56fbca36f178377ca961
David <davidarmstrong0925@gmail.com>
2025-10-05 15:22:49 -0400

    Player controller, Idamage & camera controller implemented
    Implemented player controller, created IDamage interface script, and camera controller. Player movement working. Added some simple assets and created a single room to run around in.

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour, IDamage
+{
+    [SerializeField] LayerMask ignoreLayer;
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int HP;
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int jumpCountMax;
+    [SerializeField] int gravity;
+
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDist;
+    [SerializeField] float shootRate;
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+    int jumpCount;
+    int HPorig;
+
+    float shootTimer;
+
+    bool isSprinting;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HPorig = HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+
+        sprint();
+    }
+
+    void movement()
+    {
+        if (controller.isGrounded)
+        {
+            playerVel = Vector3.zero;
+            jumpCount = 0;
+        }
+        else
+        {
+            playerVel.y -= gravity * Time.deltaTime;
+        }
+
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+        controller.Move(playerVel * Time.deltaTime);
+
+        if(Input.GetButton("Fire1") && shootTimer >= shootRate)
+        {
+            shoot();
+        }
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    void jump()
+    {
+        if ((Input.GetButtonDown("Jump")) && jumpCount < jumpCountMax)
+        {
+            playerVel.y = jumpSpeed;
+            jumpCount++;
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+        {
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+            if(dmg != null)
+            {
+                dmg.takeDamage(shootDamage);
+            }
+        }
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+
+        if(HP <= 0)
+        {
+
+        }
+    }
+}
