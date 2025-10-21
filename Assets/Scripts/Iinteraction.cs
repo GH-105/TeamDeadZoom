e9dde2c3e2c9ae1c817e876962e3e81a0ad53f6a
GH-105 <GAHernandezRivera@student.fullsail.edu>
2025-10-21 02:51:50 -0500

    new level files
    Unfortunatly none of the code worked, and the files are there but arn't opening as scenes

--- Assets/Scripts/Iinteraction.cs
+++ Assets/Scripts/Iinteraction.cs
@@ -4,15 +4,19 @@ public class Iinteraction : MonoBehaviour
 {
     [SerializeField] int speedBoost;
     [SerializeField] int jumpCIncrease;
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerController player = other.GetComponent <playerController> ();
+            gunStats gunStatP = other.GetComponent<gunStats> ();
             if (player != null)
             {
                 player.Speed += (int)speedBoost; player.JumpCountMax += (int)jumpCIncrease;
+                
+                
                 Destroy(gameObject);
             }
         }
