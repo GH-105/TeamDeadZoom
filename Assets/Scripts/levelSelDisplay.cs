792bb3fa24ca7bb33045104925cec07f6c4d316d
KFess21 <keaganfessler@gmail.com>
2025-11-01 18:45:14 -0600

    linked the times
    it saves the level time and displays that correctly on the title screen

--- Assets/Scripts/levelSelDisplay.cs
+++ Assets/Scripts/levelSelDisplay.cs
@@ -1,16 +1,37 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class levelSelDisplay : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [System.Serializable] //adds ways to connect in unity
+    public class levelUI
+    {
+        public string sceneName;
+        public TMP_Text bestTimeText;
+    }
+
+    public levelUI[] levels;
+
     void Start()
     {
-        
+        UpdateAllBestTime();    
     }
 
     // Update is called once per frame
-    void Update()
+    void UpdateAllBestTime()
     {
-        
+        foreach (var level in levels)
+        {
+            if (level.bestTimeText == null) continue; //if empty skip 
+
+            string key = "BestTime_" + level.sceneName;
+            float bestTime = PlayerPrefs.GetFloat(key, float.MaxValue);
+
+            if (bestTime == float.MaxValue)
+                level.bestTimeText.text = "--:--:--";
+            else
+                level.bestTimeText.text = TimeSpan.FromSeconds(bestTime).ToString(@"mm\:ss\:ff");
+        }
     }
 }
